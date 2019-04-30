fe6b2bf007116e1a0297a3b6ab3830d6cac1ed3c
Eovl Han <eovl.han@ericsson.com>
2019-04-30 09:12:27 +0200

    add filefunc.cpp and firstref.cpp

---
+++ study_c++/firstref.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int rats = 101;
+    int & rodents = rats;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+    rodents ++;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+
+    cout << "rats adress = " << &rats;
+    cout << ", rodents adress = " << &rodents << endl;
+    return 0;
+}
+
