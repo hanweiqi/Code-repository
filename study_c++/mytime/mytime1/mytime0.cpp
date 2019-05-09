ef51e5122e63d29b1ef7e61b296ce7403fd825b6
Eovl Han <hanweiqi3028@163.com>
2019-05-09 01:57:31 -0400

    add mytime

---
+++ study_c++/mytime/mytime1/mytime0.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "mytime0.h"
+
+Time::Time()
+{
+    hours = minutes = 0;
+}
+
+Time::Time(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+void Time::AddMin(int m)
+{
+    minutes += m;
+    hours += minutes/60;
+    hours %= 60;
+}
+
+void Time::AddHr(int h)
+{
+    hours += h;
+    hours %= 60;
+}
+
+void Time::Reset(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+Time Time::Sum(const Time & t) const
+{
+    Time sum;
+    sum.minutes = minutes + t.minutes;
+    sum.hours = hours + t.hours + sum.minutes / 60;
+    sum.hours %= 60;
+    return sum;
+}
+
+void Time::Show() const
+{
+    std::cout << hours << " hours, " << minutes << " minutes";
+}
